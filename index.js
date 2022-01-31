356885c657dab0d035f9367ea6333ed9e7f29298
david <wizdave97@gmail.com>
2022-01-31 16:32:47 +0100

    fmt

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Import the API
-const { ApiPromise, WsProvider } = require("@polkadot/api")
-const { Keyring } = require('@polkadot/keyring')
-const { cryptoWaitReady } = require('@polkadot/util-crypto')
-const { chunk } = require('lodash')
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { Keyring } = require("@polkadot/keyring");
+const { cryptoWaitReady } = require("@polkadot/util-crypto");
+const { chunk } = require("lodash");
 
-function hexToBytes (hex)  {
-  const chunks = chunk(hex.split(''), 2)
+function hexToBytes(hex) {
+  const chunks = chunk(hex.split(""), 2);
 
-  const bytes = []
-  
+  const bytes = [];
+
   for (const chunk of chunks) {
-    bytes.push(parseInt(chunk.join(''), 16));
-}
-  return bytes
+    bytes.push(parseInt(chunk.join(""), 16));
+  }
+  return bytes;
 }
 
 async function main() {
@@ -23,29 +23,35 @@ async function main() {
   // Wait for wasm to be initialized for sr25519 keys
   await cryptoWaitReady();
 
-  const keyring = new Keyring({ type: 'sr25519', ss58Format: 2 });
+  const keyring = new Keyring({ type: "sr25519", ss58Format: 2 });
 
   // Create root key
   const root = keyring.addFromUri("");
 
-  const value = [hexToBytes("0d715f2646c8f85767b5d2764bb2782604a74d81251e398fd8a0a4d55023bb3f"), hexToBytes("27080000")]
-  const values = [value]
-  const setStorageCall = api.tx.system.setStorage(values)
+  const value = [
+    hexToBytes(
+      "0d715f2646c8f85767b5d2764bb2782604a74d81251e398fd8a0a4d55023bb3f"
+    ),
+    hexToBytes("27080000"),
+  ];
+  const values = [value];
+  const setStorageCall = api.tx.system.setStorage(values);
 
-  let unsub = await api.tx.sudo.sudo(setStorageCall).signAndSend(root, ({ events = [], status }) => {
-		console.log(`Current status is: ${status.type}`);
+  let unsub = await api.tx.sudo
+    .sudo(setStorageCall)
+    .signAndSend(root, ({ events = [], status }) => {
+      console.log(`Current status is: ${status.type}`);
 
-		if (status.isFinalized) {
-			console.log(`Transaction included at blockHash ${status.asFinalized}`);
+      if (status.isFinalized) {
+        console.log(`Transaction included at blockHash ${status.asFinalized}`);
 
-			events.forEach(({ phase, event: { data, method, section } }) => {
-				console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-			});
+        events.forEach(({ phase, event: { data, method, section } }) => {
+          console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
+        });
 
-      unsub()
-		}
-	})
+        unsub();
+      }
+    });
 }
 
 main().catch(console.error);
-
